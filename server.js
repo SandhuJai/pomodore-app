5d41747a4230f614ef5e69dc52f45e468f40edb2
SandhuJai <sandhujaikirat98@gmail.com>
2018-06-13 00:31:30 +0530

    Basic Functioning Messenger App

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+const ACCESS_TOKEN = 'EAADPMq4B3BABAL6ae7nqUsdJdPqZAqOgLSwjZCEmhD0CeodCUYipKb9VSsDRpBDJKZBrVSyrPXbgZCZCYAm5HulIQ5IVB3iWa9wgCvUtOHpdHz3ZAzzcndehZApxsF1CdTbcctyuccqgdipqTMtZAoS9lL3nSyZCkrwVJAPHPWzl18nsz2V8WvS8C';
+const str = 'pomodore_app';
+
 app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
@@ -17,24 +20,48 @@ app.get('/', function (req, res) {
 });
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'pomodore_app') {
-		res.send(req.query['hub.challenge']);
-	}
-	res.send('Error, wrong token');
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.mode'] && req.query['hub.verify_token'] === str) {
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      res.status(403).end();
+    }
+  });
+
+/* Handling all messenges */
+app.post('/webhook', (req, res) => {
+    if (req.body.object === 'page') {
+        req.body.entry.forEach((entry) => {
+            entry.messaging.forEach((event) => {
+                if (event.message && event.message.text) {
+                    sendMessage(event);
+                }
+            });
+        });
+        res.status(200).end();
+    }
 });
 
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging;
-    for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i];
-	    let sender = event.sender.id;
-	    if (event.message && event.message.text) {
-		    let text = event.message.text;
-	    }
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
     }
-    res.sendStatus(200);
-});
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
