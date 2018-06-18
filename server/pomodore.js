8e155ad651b429324767ef6dfbe1d2ff50b21d3b
SandhuJai <sandhujaikirat98@gmail.com>
2018-06-18 07:26:51 +0530

    May Be ?

--- server/pomodore.js
+++ server/pomodore.js
@@ -10,13 +10,13 @@ function tick(users) {
             if(user.status === 'session') {
                 if(currTime - user.start >= user.session_length) {
                     console.log(user.id, `Session is over, time for a break of ${user.break_length/1000} seconds`);
-                    //message.sendMessage(user.id, `Session is over, time for a break of ${user.break_length/1000} seconds`);
+                    message.sendMessage(user.id, `Session is over, time for a break of ${user.break_length/1000} seconds`);
                     users.startBreak(user.id);
                 }
             }else if(user.status === 'break') {
                 if(currTime - user.start >= user.break_length) {
                     console.log(user.id, `Break is over, time for a session of ${user.session_length/1000} seconds`);
-                    //message.sendMessage(user.id, `Break is over, time for a session of ${user.session_length/1000} seconds`);
+                    message.sendMessage(user.id, `Break is over, time for a session of ${user.session_length/1000} seconds`);
                     users.startSession(user.id);
                 }
             }
