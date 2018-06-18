fd3477e1b5ac953d128b66f2f8ef40df38c1c5dd
SandhuJai <sandhujaikirat98@gmail.com>
2018-06-18 08:04:34 +0530

    Main Sanyaas Le raha hai iss dharti se

--- server/pomodore.js
+++ server/pomodore.js
@@ -14,13 +14,13 @@ function tick(usersObject) {
                 if(currTime - user.start >= user.session_length) {
                     console.log(user.id, `Session is over, time for a break of ${user.break_length/1000} seconds`);
                     message.sendMessage(user.id, `Session is over, time for a break of ${user.break_length/1000} seconds`);
-                    users.startBreak(user.id);
+                    usersObject.startBreak(user.id);
                 }
             }else if(user.status === 'break') {
                 if(currTime - user.start >= user.break_length) {
                     console.log(user.id, `Break is over, time for a session of ${user.session_length/1000} seconds`);
                     message.sendMessage(user.id, `Break is over, time for a session of ${user.session_length/1000} seconds`);
-                    users.startSession(user.id);
+                    usersObject.startSession(user.id);
                 }
             }
         }
